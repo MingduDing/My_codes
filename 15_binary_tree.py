9e3c2012098178884fc450f6fa8e5fe5a6189df4
MingduDing <dingmingdugithub@163.com>
2019-04-22 17:51:48 +0800

    Update

--- 15_binary_tree.py
+++ 15_binary_tree.py
@@ -51,19 +51,10 @@ class Tree(object):
     
     def inorder(self, node):
         """中序遍历"""
-        if node is None:
-			return
-		self.inorder(node.lchild)
-		print(node.item, end=' ')
-		self.ineorder(node.rchild)
         
     def postorder(self, node):
         """后序遍历"""
-        if node is None:
-			return
-		self.postorder(node.lchild)
-		self.postorder(node.rchild)
-		print(node.item, end=' ')
+
 
 if __name__ == "__main__":
     tree = Tree()
